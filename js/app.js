ca30384394e716733b41090c223633d909198f60
Entropic Cat University <86585719+juan2284@users.noreply.github.com>
2022-04-14 23:01:17 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,199 @@
+// Variables
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
+const max = new Date().getFullYear();
+const min = max -10;
+
+
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+  marca: '',
+  year: '',
+  minimo: '',
+  maximo: '',
+  puertas: '',
+  transmision: '',
+  color: ''
+}
+
+// Eventos
+document.addEventListener('DOMContentLoaded', () => {
+  // Muestra los autos al cargar
+  mostrarAutos(autos);
+
+  // Llenar el select de años(year)
+  llenarSelect();
+});
+
+// Event Listener para los select de búsqueda
+marca.addEventListener('change', e => {
+  datosBusqueda.marca = e.target.value;
+
+  filtrarAuto();
+});
+year.addEventListener('change', e => {
+  datosBusqueda.year = parseInt(e.target.value);
+
+  filtrarAuto();
+});
+minimo.addEventListener('change', e => {
+  datosBusqueda.minimo = e.target.value;
+
+  filtrarAuto();
+});
+maximo.addEventListener('change', e => {
+  datosBusqueda.maximo = e.target.value;
+
+  filtrarAuto();
+});
+puertas.addEventListener('change', e => {
+  datosBusqueda.puertas = parseInt(e.target.value);
+
+  filtrarAuto();
+});
+transmision.addEventListener('change', e => {
+  datosBusqueda.transmision = e.target.value;
+
+  filtrarAuto();
+});
+color.addEventListener('change', e => {
+  datosBusqueda.color = e.target.value;
+
+  filtrarAuto();
+});
+
+
+// Funciones
+function mostrarAutos(autos){
+  // Eliminar el HTML previo
+  limpiarHTML();
+
+  autos.forEach( auto => {
+    // Destructurar el arreglo
+    const {marca, modelo, year, puertas, transmision, precio, color} = auto;
+    const autoHTML = document.createElement('p');
+
+    autoHTML.textContent = `
+      ${marca} - ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Color: ${color} - Precio: $${precio}
+    `;
+
+    // Insertar en el HTML
+    resultado.appendChild(autoHTML);
+  });
+}
+
+// Función para limpiar el HTML
+function limpiarHTML(){
+  while(resultado.firstChild){
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
+// Genera los años del Select
+function llenarSelect(){
+  
+  for (let i = max; i >= min; i--) {
+    const opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.textContent = i;
+
+    // Agregar las opciones de año
+    year.appendChild(opcion);
+  }
+}
+
+
+// Función de filtrado de autos en base a la búsqueda
+function filtrarAuto(){
+  // Usaremos una función de alto nivel. Es una función que toma otra función
+  const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
+
+  // Condicional para mostrar mensaje si la búsqueda no arroja resultado
+  if(resultado.length){
+    mostrarAutos(resultado);
+  }else{
+    noResultado();
+  }
+}
+
+function filtrarMarca(auto){
+  const {marca} = datosBusqueda;
+  if(marca){
+    return auto.marca === marca;
+  }else{
+    return auto;
+  }
+}
+function filtrarYear(auto){
+  const {year} = datosBusqueda;
+
+  if(year){
+    return auto.year === year;
+  }else{
+    return auto;
+  }
+}
+function filtrarMinimo(auto){
+  const {minimo} = datosBusqueda;
+
+  if(minimo){
+    return auto.precio >= minimo;
+  }else{
+    return auto;
+  }
+}
+function filtrarMaximo(auto){
+  const {maximo} = datosBusqueda;
+
+  if(maximo){
+    return auto.precio <= maximo;
+  }else{
+    return auto;
+  }
+}
+function filtrarPuertas(auto){
+  const {puertas} = datosBusqueda;
+
+  if(puertas){
+    return auto.puertas === puertas;
+  }else{
+    return auto;
+  }
+}
+function filtrarTransmision(auto){
+  const {transmision} = datosBusqueda;
+
+  if(transmision){
+    return auto.transmision === transmision;
+  }else{
+    return auto;
+  }
+}
+function filtrarColor(auto){
+  const {color} = datosBusqueda;
+
+  if(color){
+    return auto.color === color;
+  }else{
+    return auto;
+  }
+}
+
+// Función para mostrar mensaje de que no se consiguieron resultados
+function noResultado(){
+
+  limpiarHTML();
+
+  const noResultado = document.createElement('div');
+  noResultado.classList.add('alerta', 'error');
+  noResultado.textContent = 'No hay resultados que mostrar. Intenta otra búsqueda';
+  resultado.appendChild(noResultado);
+}
